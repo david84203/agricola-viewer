9168ec05d8d7a7673dd8bc4ead5fd58ef3527a74
Lu-樓上 <david84203@gmail.com>
2026-07-05 04:55:29 +0800

    Add missing card images

--- card-images.js
+++ card-images.js
@@ -1,5 +1,5 @@
 (function () {
-  const MANIFEST_URL = './card-images.json?v=20260705-s13o2m2';
+  const MANIFEST_URL = './card-images.json?v=20260705-missing7';
   const DECKS = new Set([
     'A', 'B', 'BI', 'C', 'Cz', 'D', 'E', 'FL', 'FR', 'G', 'G4', 'G5',
     'G5+', 'G6', 'G7', 'G8', 'G9', 'HH', 'I', 'K', 'L', 'NL', 'O', 'OX',
